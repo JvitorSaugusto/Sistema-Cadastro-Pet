6069f3ef5515dc87c1a3e691b3270c60b7d18bfc
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-22 20:23:55 -0300

    feat: adicionando objeto pet

---
+++ src/br/com/joaovitor/meuprojeto/pets/domain/Pet.java
@@ -0,0 +1,34 @@
+package br.com.joaovitor.meuprojeto.pets.domain;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pet {
+    String nome;
+
+    SexoPet sexoPet;
+    //endereço
+    int idade;
+    double peso;
+    TipoPet tipoPet;
+
+    public Pet() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String regex = "[a-zA-Z]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(nome);
+        Boolean matcherCheck = matcher.find();
+
+        if (matcherCheck){
+            this.nome = nome;
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}
