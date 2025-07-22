71e23dc82d3cc8d080bbcde9ebc4087569355a94
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-22 20:24:24 -0300

    feat: adicionando enum de sexo e tipo da classe pet

---
+++ src/br/com/joaovitor/meuprojeto/pets/domain/SexoPet.java
@@ -0,0 +1,6 @@
+package br.com.joaovitor.meuprojeto.pets.domain;
+
+public enum SexoPet {
+    MACHO,
+    FEMEA
+}
---
+++ src/br/com/joaovitor/meuprojeto/pets/domain/TipoPet.java
@@ -0,0 +1,8 @@
+package br.com.joaovitor.meuprojeto.pets.domain;
+
+public enum TipoPet {
+    CACHORRO,
+    GATO,
+    PASSARO,
+    REPTIL
+}
