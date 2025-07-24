ca749f4f42992ca099e1225304c8db0ad75708e5
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-24 20:52:57 -0300

    feat: adicionando metodos e refatorando código
    - adicionado métodos perguntar sexo, endereço, idade, peso, para limpar o código
    - mudança do nome da variavel map para algo mais claro, mapDeRespostas
    - limpeza de código
    - Otimização dos prints das perguntas para usuarios

--- src/br/com/joaovitor/meuprojeto/main/MainTest.java
+++ src/br/com/joaovitor/meuprojeto/main/MainTest.java
@@ -2,6 +2,11 @@ package br.com.joaovitor.meuprojeto.main;
 
 import br.com.joaovitor.meuprojeto.files.test.FileReadingTest01;
 import br.com.joaovitor.meuprojeto.files.test.FileWriteTest01;
+import br.com.joaovitor.meuprojeto.menu.methods.Submenu02;
+import br.com.joaovitor.meuprojeto.menu.methods.Submenu03;
+import br.com.joaovitor.meuprojeto.menu.methods.Submenu04;
+import br.com.joaovitor.meuprojeto.menu.methods.Submenu05;
+import br.com.joaovitor.meuprojeto.pets.domain.PetBox;
 
 import java.util.*;
 
@@ -10,91 +15,51 @@ public class MainTest {
         FileWriteTest01.writing();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bem vindo ao menu, escolha uma das opções abaixo:");
-        System.out.println("1.Cadastrar um novo pet\n2.Alterar os dados do pet cadastrado\n3.Deletar um pet cadastrado\n" +
-                "4.Listar todos os pets cadastrados\n5.Listar pets por algum critério (idade, nome, raça)\n6.Sair");
 
-        int opcaoMenuParse = -1;
+        while (true) {
+            System.out.println("Bem vindo ao menu, escolha uma das opções abaixo:");
+            System.out.println("1.Cadastrar um novo pet\n2.Alterar os dados do pet cadastrado\n3.Deletar um pet cadastrado\n" +
+                    "4.Listar todos os pets cadastrados\n5.Listar pets por algum critério (idade, nome, raça)\n6.Sair");
 
-        while (opcaoMenuParse != 6) {
             String opcaoMenu = scanner.next();
             scanner.nextLine();
-            String resposta = "";
+            String resposta;
 
             try {
-                opcaoMenuParse = Integer.parseInt(opcaoMenu);
+                int opcaoMenuParse = Integer.parseInt(opcaoMenu);
                 if (opcaoMenuParse == 1) {
-                    Map<String, String> map = new HashMap<>();
+                    Map<String, String> mapDeRespostas = new HashMap<>();
                     List<String> chaveMap = new ArrayList<>();
                     List<String> perguntas = FileReadingTest01.Reading();
-                    List<String> respostas = new ArrayList<>();
 
                     for (int i = 0; i < perguntas.size(); i++) {
                         System.out.println(perguntas.get(i));
                         if (i == 2) {
-                            // dps transformar em um metodo "perguntarSexo"
-                            System.out.println("Digite: MACHO ou FEMEA");
-                            resposta = scanner.nextLine().trim().toUpperCase();
-                            if (!resposta.equals("MACHO") && !resposta.equals("FEMEA")) {
-                                System.out.println("ERRO: Valor invalido");
-                                i--;
-                                continue;
-                            }
+                            resposta = Submenu02.perguntarSexo(scanner);
                         } else if (i == 3) {
-                            // dps transformar em um metodo "perguntarEndereço"
-                            System.out.println("Qual o Número da casa?");
-                            int numero = scanner.nextInt();
-                            String numeroConvertido = Integer.toString(numero);
-                            // tentar validar se cidade contem apenas letras, validar com regex
-                            System.out.println("Qual a Cidade?");
-                            String cidade = scanner.nextLine();
-                            System.out.println("Qual a Rua?");
-                            String rua = scanner.nextLine();
-                            resposta = numeroConvertido + "," + rua + "," + cidade;
-
+                            resposta = Submenu03.perguntarEndereco(scanner);
                         } else if (i == 4) {
-                            String idadeInput = scanner.nextLine();
-                            if (idadeInput.trim().isEmpty()){
-                                resposta = "NÃO INFORMADO";
-                            }
-                            Double idade = Double.parseDouble(idadeInput);
-                            if (idade > 20 || idade < 0.5) {
-                                throw new IllegalArgumentException("Argumento ilegal, não pode ser mais que 20");
-                            } else if (idade <= 20) {
-                                System.out.println("A idade sera em meses ou anos?");
-                                String inputIdade = scanner.nextLine().toLowerCase();
-                                if (inputIdade.equals("meses")) {
-                                    idade /= 12.0;
-                                    resposta = String.valueOf(idade);
-                                } else if (inputIdade.equals("anos")){
-                                    resposta = String.valueOf(idade);
-                                } else{
-                                    System.out.println("Selecione uma opção válida.");
-                                    continue;
-                                }
-                            }
-                            
+                            resposta = Submenu04.perguntaIdade(scanner);
                         } else if (i == 5) {
-                            double peso = scanner.nextInt();
-                            if (peso < 1 && peso > 0.5) {
-
-                            } else if (peso > 60 || peso < 0.5) {
-                                throw new IllegalArgumentException("Argumento ilegal, não pode ser maior que 60 kg e menos que 0.5 kg");
-                            }
+                            resposta = Submenu05.perguntaPeso(scanner);
                         } else {
                             resposta = scanner.nextLine().trim();
                         }
                         chaveMap.add(Integer.toString(i));
-                        map.put(chaveMap.get(i), resposta);
+                        mapDeRespostas.put(chaveMap.get(i), resposta);
                     }
-                    System.out.println("Inserindo respostas na lista");
-                    respostas.addAll(map.values());
+
+                    List<String> respostas = new ArrayList<>(mapDeRespostas.values());
+
+                    System.out.println("Adicionando pets na lista");
+                    PetBox.guardaPet(respostas);
 
                 } else if (opcaoMenuParse == 2) {
                     System.out.println("...");
                 } else if (opcaoMenuParse <= 0) {
                     System.out.println("Digite somente numeros válidos");
                 } else if (opcaoMenuParse == 6) {
+                    System.err.println("Saindo do sistema!");
                     break;
                 }
             } catch (NumberFormatException e) {
