04f2e06bd526f201cb862765754271b33de19e8f
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-24 12:26:30 -0300

    feat: adiciona novo atributo, set e comentarios
    - Adiciona novo atributo de endereço
    - Adiciona novos sets no fluxo
    - Adiciona comentários para melhorias futuras

--- src/br/com/joaovitor/meuprojeto/pets/domain/Pet.java
+++ src/br/com/joaovitor/meuprojeto/pets/domain/Pet.java
@@ -7,7 +7,7 @@ public class Pet {
     String nome;
     TipoPet tipoPet;
     SexoPet sexoPet;
-    //endereço
+    String endereco;
     int idade;
     double peso;
     String raca;
@@ -21,6 +21,7 @@ public class Pet {
     }
 
     public void setNome(String nome) {
+        // isso aqui pode virar um metodo para verificar se contém somente letras
         String regex = "[a-zA-Z]";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(nome);
@@ -31,5 +32,25 @@ public class Pet {
         } else {
             throw new RuntimeException();
         }
+    }
+
+    public void setTipoPet(TipoPet tipoPet) {
+        this.tipoPet = tipoPet;
+    }
+
+    public void setSexoPet(SexoPet sexoPet) {
+        this.sexoPet = sexoPet;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
     }
 }
