98fd1cd7b49ffa04ce0a9ee77a8248a981373e0d
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-24 09:58:51 -0300

    feat: adicionando List para iniciar a lógica de menu

--- src/br/com/joaovitor/meuprojeto/files/test/FileReadingTest01.java
+++ src/br/com/joaovitor/meuprojeto/files/test/FileReadingTest01.java
@@ -1,19 +1,24 @@
 package br.com.joaovitor.meuprojeto.files.test;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FileReadingTest01 {
-    public static void Reading() {
+    public static List <String> Reading() {
         File file = new File("formulario.txt");
+        List <String> perguntas = new ArrayList<>();
+
         try(FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr)) {
             String linha;
             while((linha = br.readLine()) != null){
-                System.out.println(linha);
+                perguntas.add(linha);
             }
         } catch (IOException e) {
             System.err.println("Erro:" + e.getMessage());
         }
+        return perguntas;
     }
 }
