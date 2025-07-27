8ed10a94c9a79cfe5e1c8212ee3ad7d6c611cf76
Jvitor <99294410+JvitorSaugusto@users.noreply.github.com>
2025-07-27 15:31:22 -0300

    feat: criando a classe que salva respostas
    - criando método de gerar nomes dinâmicos com base na hora
    - criando toda a lógica de escrita dos dados respondidos dos usuarios em um txt

---
+++ src/br/com/joaovitor/meuprojeto/files/test/WriterResponseFile.java
@@ -0,0 +1,37 @@
+package br.com.joaovitor.meuprojeto.files.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class WriterResponseFile {
+    public static void escrevendoRespostas(List<String> respostas) {
+        File fileDiretorio = new File("petsCadastrados");
+        File file = new File(fileDiretorio, geradorNomeDinamico(respostas.getFirst()));
+        try {
+            boolean isFileCreated = file.createNewFile();
+            System.out.println(isFileCreated);
+        } catch (IOException e) {
+            System.err.println("Erro:" + e.getMessage());
+        }
+
+        try (FileWriter fw = new FileWriter(file);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            FileWriteTest01.escreverPulandoLinha(respostas, bw);
+            bw.flush();
+        } catch (IOException e) {
+            System.err.println("Erro:" + e.getMessage());
+        }
+    }
+
+    public static String geradorNomeDinamico(String nomeCompleto) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        String localDateTimeBR = LocalDateTime.now().format(formatter);
+        return localDateTimeBR + nomeCompleto + ".txt";
+    }
+}
+
